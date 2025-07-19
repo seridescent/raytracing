dacbd7ad017e2bc473d55aaadd8b5ed88bdbacf3
seridescent <seridescent@gmail.com>
2025-07-19 18:43:53 -0400

    feat(3.1): basic vector implementation and ppm writing util

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
+use crate::vector::Vector3;
+
+pub mod vector;
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
 
-    println!("P3\n{image_width} {image_height}\n255");
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("{}", 255);
 
     for row in 0..image_height {
+        eprint!("\rScanlines remaining: {}", image_height - row);
         for col in 0..image_width {
-            let r = col as f64 / f64::from(image_width - 1);
-            let g = row as f64 / f64::from(image_height - 1);
-            let b = 0 as f64;
+            let color = Vector3 {
+                x: col as f64 / f64::from(image_width - 1),
+                y: row as f64 / f64::from(image_height - 1),
+                z: 0 as f64,
+            };
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{ir} {ig} {ib}");
+            println!("{}", ppm_pixel(color))
         }
     }
+}
+
+fn ppm_pixel(color: Vector3) -> String {
+    let ir = (255.999 * color.x) as u8;
+    let ig = (255.999 * color.y) as u8;
+    let ib = (255.999 * color.z) as u8;
+
+    format!("{ir} {ig} {ib}")
 }
---
+++ src/vector.rs
@@ -0,0 +1,133 @@
+use std::ops;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vector3 {
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn to_unit(self) -> Self {
+        self / self.length()
+    }
+}
+
+impl ops::Add for Vector3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::AddAssign for Vector3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl ops::Mul for Vector3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vector3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl ops::MulAssign for Vector3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+
+impl ops::MulAssign<f64> for Vector3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl ops::Div<f64> for Vector3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self {
+        let inv = 1.0 / rhs;
+        Self {
+            x: self.x * inv,
+            y: self.y * inv,
+            z: self.z * inv,
+        }
+    }
+}
+
+impl ops::DivAssign<f64> for Vector3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self *= 1.0 / rhs;
+    }
+}
+
+impl ops::Sub for Vector3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::SubAssign for Vector3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl ops::Neg for Vector3 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
