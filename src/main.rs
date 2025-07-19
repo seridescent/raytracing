9126cb97d7aa9743bc9e4adb383bcaed5faa0d40
seridescent <seridescent@gmail.com>
2025-07-19 14:40:59 -0400

    feat(2.2): write simple ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{image_width} {image_height}\n255");
+
+    for row in 0..image_height {
+        for col in 0..image_width {
+            let r = col as f64 / f64::from(image_width - 1);
+            let g = row as f64 / f64::from(image_height - 1);
+            let b = 0 as f64;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
