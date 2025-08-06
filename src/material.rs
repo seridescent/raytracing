4227188eb69c815b672ec631b5dead731591371b
seridescent <seridescent@gmail.com>
2025-08-06 15:48:01 -0400

    style: break up scatter implementation

--- src/material.rs
+++ src/material.rs
@@ -1,10 +1,4 @@
-use rand::random;
-
-use crate::{
-    hittable::Hit,
-    ray::Ray,
-    vector::{Vector3, dot, reflect, refract},
-};
+use crate::{hittable::Hit, ray::Ray, vector::Vector3};
 
 #[derive(Clone, Debug)]
 pub struct Scatter {
@@ -22,59 +16,91 @@ pub enum Material {
 impl Material {
     pub fn scatter(&self, ray: &Ray, hit: &Hit) -> Option<Scatter> {
         match *self {
-            Material::Lambertian { albedo } => {
-                let direction = hit.face_normal + Vector3::random_unit();
-                let direction = if direction.is_near_zero() {
-                    hit.face_normal
-                } else {
-                    direction
-                };
-
-                Some(Scatter {
-                    ray: Ray::new(hit.p, direction),
-                    attenuation: albedo,
-                })
-            }
+            Material::Lambertian { albedo } => lambertian::scatter(albedo, ray, hit),
             Material::Metal {
                 albedo,
                 fuzz_radius,
-            } => {
-                let reflected = reflect(ray.direction, hit.face_normal);
-                let fuzz = Vector3::random_unit() * fuzz_radius;
-                let fuzzed = reflected.to_unit() + fuzz;
-                if dot(fuzzed, hit.face_normal) > 0.0 {
-                    Some(Scatter {
-                        ray: Ray::new(hit.p, fuzzed),
-                        attenuation: albedo,
-                    })
-                } else {
-                    None
-                }
-            }
+            } => metal::scatter(albedo, fuzz_radius, ray, hit),
             Material::Dielectric { refraction_index } => {
-                let r_in = ray.direction.to_unit();
-                let eta_in_over_eta_out = if hit.front_face {
-                    1.0 / refraction_index
-                } else {
-                    refraction_index
-                };
-
-                let cos_theta = dot(-r_in, hit.face_normal).clamp(-1.0, 1.0);
-                let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
-                let r_out = if eta_in_over_eta_out * sin_theta > 1.0
-                    || reflectance(cos_theta, eta_in_over_eta_out) > random::<f64>()
-                {
-                    reflect(r_in, hit.face_normal)
-                } else {
-                    refract(r_in, hit.face_normal, eta_in_over_eta_out)
-                };
-
-                Some(Scatter {
-                    ray: Ray::new(hit.p, r_out),
-                    attenuation: Vector3::new(1.0, 1.0, 1.0),
-                })
+                dielectric::scatter(refraction_index, ray, hit)
             }
         }
+    }
+}
+
+mod lambertian {
+    use super::Scatter;
+    use crate::{hittable::Hit, ray::Ray, vector::Vector3};
+
+    pub fn scatter(albedo: Vector3, _ray: &Ray, hit: &Hit) -> Option<Scatter> {
+        let direction = hit.face_normal + Vector3::random_unit();
+        let direction = if direction.is_near_zero() {
+            hit.face_normal
+        } else {
+            direction
+        };
+
+        Some(Scatter {
+            ray: Ray::new(hit.p, direction),
+            attenuation: albedo,
+        })
+    }
+}
+
+mod metal {
+    use super::Scatter;
+    use crate::{
+        hittable::Hit,
+        ray::Ray,
+        vector::{Vector3, dot, reflect},
+    };
+
+    pub fn scatter(albedo: Vector3, fuzz_radius: f64, ray: &Ray, hit: &Hit) -> Option<Scatter> {
+        let reflected = reflect(ray.direction, hit.face_normal);
+        let fuzz = Vector3::random_unit() * fuzz_radius;
+        let fuzzed = reflected.to_unit() + fuzz;
+        if dot(fuzzed, hit.face_normal) > 0.0 {
+            Some(Scatter {
+                ray: Ray::new(hit.p, fuzzed),
+                attenuation: albedo,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+mod dielectric {
+    use super::{Scatter, reflectance};
+    use crate::{
+        hittable::Hit,
+        ray::Ray,
+        vector::{Vector3, dot, reflect, refract},
+    };
+    use rand::random;
+
+    pub fn scatter(refraction_index: f64, ray: &Ray, hit: &Hit) -> Option<Scatter> {
+        let r_in = ray.direction.to_unit();
+        let eta_in_over_eta_out = if hit.front_face {
+            1.0 / refraction_index
+        } else {
+            refraction_index
+        };
+
+        let cos_theta = dot(-r_in, hit.face_normal).clamp(-1.0, 1.0);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+        let r_out = if eta_in_over_eta_out * sin_theta > 1.0
+            || reflectance(cos_theta, eta_in_over_eta_out) > random::<f64>()
+        {
+            reflect(r_in, hit.face_normal)
+        } else {
+            refract(r_in, hit.face_normal, eta_in_over_eta_out)
+        };
+
+        Some(Scatter {
+            ray: Ray::new(hit.p, r_out),
+            attenuation: Vector3::new(1.0, 1.0, 1.0),
+        })
     }
 }
 
